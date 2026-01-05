adbed1e522fc1adb7af5c92ed5663f6809fef6dc
Amannpy <anujgautam0508@gmail.com>
2026-01-06 00:59:00 +0530

    feat(api): add REST views to create and retrieve download jobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import status, generics
+from rest_framework.response import Response
 
-# Create your views here.
+from jobs.models import DownloadJob
+from jobs.serializers import (
+    DownloadJobCreateSerializer,
+    DownloadJobSerializer,
+)
+from jobs.tasks import process_download_job
+
+
+class DownloadJobCreateAPIView(generics.CreateAPIView):
+    queryset = DownloadJob.objects.all()
+    serializer_class = DownloadJobCreateSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        job = serializer.save(status="PENDING")
+
+        # Enqueue async processing
+        process_download_job.delay(str(job.id))
+
+        return Response(
+            DownloadJobSerializer(job).data,
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class DownloadJobListAPIView(generics.ListAPIView):
+    queryset = DownloadJob.objects.all().order_by("-created_at")
+    serializer_class = DownloadJobSerializer
+
+
+class DownloadJobDetailAPIView(generics.RetrieveAPIView):
+    queryset = DownloadJob.objects.all()
+    serializer_class = DownloadJobSerializer
